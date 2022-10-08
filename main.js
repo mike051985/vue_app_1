60f3cccf471feb774ac7320117f12de60a87057e
Michelet Saint-Preux <michelet2000@gmail.com>
2022-10-09 03:56:38 +0800

    index.html main.js style.css

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const app = Vue.createApp({
     data: function() {
         return {
+            cart: 0,
+            image: 'https://i.postimg.cc/Y9YKdC0w/socks-green.jpg',
             product: 'Socks',
-            image: 'https://i.postimg.cc/Y9YKdC0w/socks-green.jpg'
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green', image: 'https://i.postimg.cc/Y9YKdC0w/socks-green.jpg'},
+                {id: 2235, color: 'blue', image: 'https://i.postimg.cc/BbMKWb1k/socks-blue.jpg'}
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
