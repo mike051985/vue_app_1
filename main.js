18404b3693666b64c9ab8002ae6fca7274d661ab
Michelet Saint-Preux <michelet2000@gmail.com>
2022-10-08 17:34:20 +0800

    index.html main.js style.css

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            image: 'https://i.postimg.cc/Y9YKdC0w/socks-green.jpg'
+        }
+    }
+})
